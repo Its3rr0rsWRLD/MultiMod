5ffec663e6f5bee24f64f6c8623df0c6da21125e
Error 404 <92172873+ThatError404@users.noreply.github.com>
2022-07-28 00:38:12 -0500

    Update Mods.mm

--- ServerFiles/Games/Minecraft/Mods/Mods.mm
+++ ServerFiles/Games/Minecraft/Mods/Mods.mm
@@ -1,1 +1,1 @@
-%TITLE:Forge Loader% %DESC:Minecraft Mod Client% %REQ:None%
+%TITLE:Forge Loader% %DESC:Minecraft Mod Client% %REQ:None% %FILE:https://raw.githubusercontent.com/ThatError404/MultiMod/main/ServerFiles/Games/Minecraft/Mods/Forge-Loader/installer.py%
