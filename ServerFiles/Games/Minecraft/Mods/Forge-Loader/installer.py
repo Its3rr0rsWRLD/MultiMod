ed7d318bfd0df05f6faa91174287a51a6c9f7350
Error 404 <92172873+ThatError404@users.noreply.github.com>
2022-07-28 01:22:56 -0500

    Update installer.py

--- ServerFiles/Games/Minecraft/Mods/Forge-Loader/installer.py
+++ ServerFiles/Games/Minecraft/Mods/Forge-Loader/installer.py
@@ -2,6 +2,9 @@ import urllib.request
 import sys
 import os
 
+if not os.path.exists("/usr/local/bin/MultiMod/Temp/Forge"):
+    os.makedir("/usr/local/bin/MultiMod/Temp/Forge")
+
 print("Please wait while installing Forge.\n\nThis will launch 1 program. Please install using 'client side', this is selected as default.\n\nThis will install Forge 1.19.1")
 urllib.request.urlretrieve("https://maven.minecraftforge.net/net/minecraftforge/forge/1.19-41.1.0/forge-1.19-41.1.0-installer.jar", "/usr/local/bin/MultiMod/Temp/Forge/forge-1.19-41.1.0-installer.jar")
 os.system("java -jar /usr/local/bin/MultiMod/Temp/Forge/forge-1.19-41.1.0-installer.jar")
